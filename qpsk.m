100502b4373022d150f92076e1ae92ea8bbf271c
Dušan Jurica <dusan.jurica@gmail.com>
2011-11-19 16:44:59 +0100

    QPSK modulátor, s pomocí NRZ kodéru. NRZ kodér je stažený někde z internetu

---
+++ qpsk.m
@@ -0,0 +1,101 @@
+%QPSK modulator%
+
+clear all
+close all
+clc
+
+N=500; % Pocet datovych bitu
+data = randsrc(1,N,[0 1]); % Generuje nahodna data podle slovniku [-1,1]
+Rb = 1e3; % Bitrate
+amplitude = 1;
+
+
+% Rozlisi liche a sude datove bity
+oddBits = data(1:2:end);
+evenBits = data(2:2:end);
+
+figure()
+subplot(311)
+title('Pure data bits')
+stem(data)
+% Limity rozsahu os musi byt za volanim funkce stem(), jinak to nefunguje
+ylim([-0.3 1.2])
+
+Fc = 2*Rb; % Kmitocet nosne vlny
+
+% Zakodovana data pomoci skriptu NRZ encoder
+[evenTime,evenNrzData,Fs]=NRZ_Encoder(evenBits,Rb,amplitude,'Polar');
+[oddTime,oddNrzData]=NRZ_Encoder(oddBits,Rb,amplitude,'Polar');
+
+% Vytvori se synfazni a kvadraturni slozka
+inPhaseOsc = 1/sqrt(2)*cos(2*pi*Fc*evenTime);
+quadPhaseOsc = 1/sqrt(2)*sin(2*pi*Fc*oddTime);
+
+% Vektor QPSK signalu
+qpskModulated = oddNrzData.*quadPhaseOsc + evenNrzData.*inPhaseOsc;
+
+% Vizualizace dat
+Tb=1/Rb;
+subplot(312);
+stem(data);
+xlabel('Samples');
+ylabel('Amplitude');
+title('Input Binary Data');
+axis([0,N,-0.5,1.5]);
+
+subplot(313);
+% [todo] nastudovat funkci plotHandle()
+% Funkce plot vraci hodnotu, funkce plotHandle se pak dale asi vyuzije
+plotHandle=plot(qpskModulated);
+xlabel('Samples');
+ylabel('Amplitude');
+title('QPSK modulated Data');
+xlimits = XLIM;   
+ylimits = YLIM;
+axis([xlimits,ylimits(1)-0.5,ylimits(2)+0.5]) ;
+grid on;
+
+% Ted uz mame QPSK signal, ktery nese informaci.
+% Udelame rychlou fourierovu transformaci
+
+fftqpskmodulated = abs(fft(qpskModulated));
+% fftqpskmodulated = filter([0.2 0.2 0.2 0.2 0.2], 1, fftqpskmodulated);
+figure()
+subplot(211)
+plot(fftqpskmodulated);
+title('fft of qpsk modulated data');
+xlabel('frequency [Hz]');
+ylabel('value [V]');
+grid on;
+
+subplot(212)
+% Vyfiltrovat signal filtrem s koeficienty 1/N delky N
+fftqpskmodulated = filter([0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 ...
+    0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 0.05 ], 1, fftqpskmodulated);
+plot(fftqpskmodulated);
+title('fft of qpsk modulated data');
+xlabel('frequency [Hz]');
+ylabel('value [V]');
+grid on;
+
+% ROC krivky
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
