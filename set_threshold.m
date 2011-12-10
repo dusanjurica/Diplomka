323f5bcc5659d18af8fc30de6e0459b121963817
Dušan Jurica <dusan.jurica@gmail.com>
2011-12-10 15:45:55 +0100

    set_threshold
    Vylepseny algoritmus prahovani

--- set_threshold.m
+++ set_threshold.m
@@ -1,7 +1,16 @@
 function [new_threshold]=set_threshold(old_threshold, direction)
-    if (direction == '+')
-        new_threshold = old_threshold + 0.1;
-    elseif (direction == '-')
-        new_threshold = oldthreshold - 0.1;
+
+    switch direction
+        case '+'
+            new_threshold = old_threshold + 0.1;
+        case '-'
+            new_threshold = old_threshold - 0.1;
+        case '++'
+            new_threshold = old_threshold + 0.5;
+        case '--'
+            new_threshold = old_threshold - 0.5;
+        otherwise
+            new_threshold = old_threshold;
     end
+    
 end
